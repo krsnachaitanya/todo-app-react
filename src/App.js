0f7415c92e44b433d1c6662973bdc31231b3f9c4
Chaitu <chaitu.ca539@gmail.com>
2020-10-07 01:46:35 +0530

    added form to input and add todos to the array

--- src/App.js
+++ src/App.js
@@ -6,10 +6,23 @@ function App() {
     "Take dogs for a walk",
     "Take the rubbish out",
   ]);
+  const [input, setInput] = useState("");
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, input]);
+    setInput("");
+  };
   return (
     <div className="App">
-      <input type="text" name="todo" />
-      <button type="submit">Add Todo</button>
+      <form action="" method="post">
+        <input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button onClick={addTodo} type="submit">
+          Add Todo
+        </button>
+      </form>
       <ul>
         {todos.map((todo) => (
           <li>{todo}</li>
